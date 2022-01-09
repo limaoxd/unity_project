4935de021ba19f9f6ee74d4c261a2d81edc1c487
hyi1014 <hyi28915088@gmail.com>
2022-01-10 04:45:54 +0800

    update_part_2

--- Project/Assets/Scripts/Boss_gate.cs
+++ Project/Assets/Scripts/Boss_gate.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class Boss_gate : MonoBehaviour
 {
-    public GameObject boss_spawner;
-    public string BossName;
-    private GameObject boss;
+    public GameObject[] boss_spawner;
+    public string[] BossName;
+    private GameObject[] boss;
     public void OnTriggerExit(Collider other)
     {
         foreach(var it in GetComponents<BoxCollider>())
             it.enabled = !it.enabled;
-        boss_spawner.GetComponent<spawner>().reset = true;
+        foreach(var it in boss_spawner)
+            it.GetComponent<spawner>().reset = true;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        boss = new GameObject[BossName.Length];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!boss && GameObject.Find(BossName)) boss = GameObject.Find(BossName);
+        int count = 0;
 
-        if(boss && boss.GetComponent<AI>().dead)  Object.Destroy(this.gameObject);
+        for(int i = 0 ;i < BossName.Length ; i++){
+            if(!boss[i] && GameObject.Find(BossName[i])) boss[i] = GameObject.Find(BossName[i]);
+            if(boss[i] && boss[i].GetComponent<AI>().dead) count ++;
+        }
+    
+        if(count == BossName.Length)  Object.Destroy(this.gameObject);
     }
 }
