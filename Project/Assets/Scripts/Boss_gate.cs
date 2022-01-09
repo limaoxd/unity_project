7dd99249c4b8cbecfa03b4f106e1d2258d58f401
limaox <limaoxgood@gmail.com>
2022-01-10 01:39:18 +0800

    0.6.0.3_Version boss_gate shotdown

--- Project/Assets/Scripts/Boss_gate.cs
+++ Project/Assets/Scripts/Boss_gate.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Boss_gate : MonoBehaviour
 {
+    public GameObject boss_spawner;
     public string BossName;
-
-    public GameObject boss;
-
+    private GameObject boss;
     public void OnTriggerExit(Collider other)
     {
         foreach(var it in GetComponents<BoxCollider>())
             it.enabled = !it.enabled;
-        GameObject.Find("Boss_Spawner").GetComponent<spawner>().reset = true;
+        boss_spawner.GetComponent<spawner>().reset = true;
     }
 
     // Start is called before the first frame update
